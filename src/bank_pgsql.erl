5f632aff1faedadc01878df74c207d69cf085d27
Tom Burdick <thomas.burdick@gmail.com>
2013-06-12 12:28:52 -0500

    Some basic send with timeout functionality and general flow of connect

--- src/bank_pgsql.erl
+++ src/bank_pgsql.erl
@@ -30,17 +30,33 @@
 -behaviour(bank_client).
 
 -record(state, {
+	timeout = undefined :: non_neg_integer(),
+	host = undefined :: string(),
+	port = undefined :: integer(),
+	user = undefined :: string(),
+	password = undefined ::string(),
+	database = undefined :: string(),
 	socket = undefined :: undefined
 }).
+
+-define(int16, 1/big-signed-unit:16).
+-define(int32, 1/big-signed-unit:32).
+
 
 %% @doc Connect to a postgresql database.
 %% By default this will connect to a localhost running postgresql server using
 %% trusted auth
 %% @end
 -spec connect(bank_client:opts()) -> {ok, bank_client:state()}
-	| {error, timeout | auth | network, string()}.
-connect(_Opts) ->
-	{ok, #state{}}.
+	| {error, opts | timeout | auth | inet:posix(), string()}.
+connect(Opts) ->
+	case parse_opts(Opts) of
+		{ok, State} ->
+			connect_begin(State);
+		Other ->
+			Other
+	end.
+
 
 %% @doc Close connection to postgresql server if its connected.
 -spec close(bank_client:state()) -> {ok, bank_client:state()}.
@@ -96,3 +112,97 @@ commit_transaction(State) ->
 	bank_client:error_result().
 rollback_transaction(State) ->
 	{ok, State}.
+
+
+
+%% Connection Steps
+
+%% @private
+%% @doc Parse the options proplist and fill out the state record. This
+%% checks for option errors in a reasonable way.
+%% @end
+parse_opts(Opts) ->
+	State = #state {
+		timeout = proplists:get_value(timeout, Opts, 5000),
+		host = proplists:get_value(host, Opts, "localhost"),
+		port = proplists:get_value(port, Opts, 5432),
+		user = proplists:get_value(user, Opts),
+		password = proplists:get_value(password, Opts),
+		database = proplists:get_value(database, Opts)
+	},
+	validate_user_opt(State).
+
+%% @private
+%% @doc Validate the user option exists
+validate_user_opt(#state{user=undefined}) ->
+	{error, opts, "Missing user connection option"};
+validate_user_opt(State) ->
+	State.
+
+
+%% @private
+%% @doc Connect to the database over a socket
+connect_begin(State=#state{timeout=Timeout, host=Host, port=Port}) ->
+	case gen_tcp:connect(Host, Port, [binary, {active, false}, {packet, raw}], Timeout) of
+		{ok, Socket} ->
+			connect_send_auth(State#state{socket=Socket});
+		_ ->
+			Message = io_lib:format("Failed to connect to host ~p on port ~p",
+				[Host, Port]),
+			{error, network, Message}
+	end.
+
+%% @private
+%% @doc Send the authentication message
+connect_send_auth(State=#state{socket=Socket, user=User, database=Database, timeout=Timeout}) ->
+	Msg = ["user", 0, User, 0],
+	Msg0 = case Database of
+		"" ->
+			Msg;
+		undefined ->
+			Msg;
+		_ ->
+			Msg ++ ["database", 0, Database, 0]
+	end,
+	case send_msg(Socket, Timeout, 0, Msg0) of
+		ok ->
+			connect_recv_auth(State);
+		Other ->
+			Other
+	end.
+
+%% @private
+%% @doc Wait for authentication message reply, send authentication password
+%% if needed and authentication method is supported (trust, md5)
+connect_recv_auth(State) ->
+	{ok, State}.
+
+%% @private 
+%% @doc Encode a message with a type and send it with a timeout
+send_msg(Socket, Timeout, Type, Msg) ->
+	Msg0 = iolist_to_binary(Msg),
+	Msg1 = <<Type:8, (byte_size(Msg0) + 4):?int32, Msg0/binary>>,
+	send_msg(Socket, Timeout, Msg1).
+
+%% @private 
+%% @doc Send a msg on a socket with a timeout.
+send_msg(Socket, Timeout, Msg) ->
+	case inet:setopts(Socket, [{send_timeout, Timeout}, {send_timeout_close, true}]) of
+		ok ->
+			send_data(Socket, Msg);
+		{error, Reason} ->
+			{fatal, Reason, "Failed to set socket timeout options"}
+	end.
+
+%% @private
+%% @doc Send data out on a socket
+send_data(Socket, Data) ->
+	case gen_tcp:send(Socket, Data) of
+		ok ->
+			ok;
+		{error, timeout} ->
+			{fatal, timeout, "Socket send timed out"};
+		{error, Reason} ->
+			{error, Reason, "Socket send failed"}
+	end.
+
